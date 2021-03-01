1fa2289120e30f852b3ea60c8adc7e500f9ed3da
Andres <gonzalezaj.mat@gmail.com>
2021-03-01 13:27:00 -0500

    el 2 no puede ser dividido en 2 partes pares (era la trampita jaja)

--- Awatermelon.cpp
+++ Awatermelon.cpp
@@ -1,10 +1,11 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 int main()
 {
     int w;
     cin>>w;
-    if(w%2==0)
+    if(w%2==0 && w!=2)
         cout<<"YES";
     else
         cout<<"NO";
